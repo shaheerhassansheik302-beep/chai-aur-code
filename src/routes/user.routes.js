4bb2725dcca18492778f24caf5ba5fd7bce4bb8f
shaheer2005 <hshaheer40@gmail.com>
2025-10-04 22:50:39 +0500

    refresh tokens

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/user.controller.js"
+import { loginUser, registerUser, refreshAccessToken } from "../controllers/user.controller.js"
 import { upload } from "../middlewares/multer.middleware.js"
 const router = Router();
 
@@ -15,4 +15,8 @@ router.route("/register").post(
         }
     ]),
     registerUser)
+
+router.route("/login").post(loginUser)
+router.route("/logout").post(verifyJWT, logoutUser)
+router.route("/refresh-token").post(refreshAccessToken)
 export default router;
